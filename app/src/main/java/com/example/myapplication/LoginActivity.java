7edb0b111e798a503e6fc8b7f9835b4eb6f356d2
Suriya Stephen <suriyanstephen@gmail.com>
2019-12-09 17:31:51 +0530

    first commit

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,30 +2,32 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-//import retrofit2.Retrofit;
+import com.example.myapplication.HomeActivity;
+import com.example.myapplication.R;
+
+import com.google.android.material.tabs.TabLayout;
+
 
 public class LoginActivity extends AppCompatActivity {
     /**
         Method definition     **/
 
-
-    protected void onCreate(@Nullable Bundle x) {
+    protected void onCreate(@Nullable Bundle x)
+    {
         super.onCreate(x);
         setContentView(R.layout.activity_login);//Android method
 
-    }  /* final EditText etUsername=findViewById(R.id.editText);
-
-
-
-
-
+        final EditText etUsername=findViewById(R.id.editText);
         final EditText etPassword=findViewById(R.id.editText1);
         Button btnLogin=findViewById(R.id.btnLogin);
 
@@ -37,27 +39,23 @@ public class LoginActivity extends AppCompatActivity {
                 String username= etUsername.getText().toString();
                 String password= etPassword.getText().toString();
 
-                // TODO Perform User Validation
+                if(username.equals("username")&& password.equals("password"))
+                {
 
-               /* Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl("https://api.github.com/")
-                        .build();*/
-
-                /** Toast
-                    .makeText(getApplicationContext(),username,Toast.LENGTH_SHORT)
-                    .show();//Method Chaining
-                 Toast
-                    .makeText(getApplicationContext(),password,Toast.LENGTH_LONG)
-                    .show(); **/
-
-
-            }//
-       // });//
+                    Intent intent=new Intent(getApplicationContext(), HomeActivity.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    Toast
+                            .makeText(getApplicationContext(), "Invalid Credentials", Toast.LENGTH_SHORT)
+                            .show();
+                }
 
 
-   /** public void processSignupLinkClick(View view)
-    {
-        Intent intent=new Intent(this,RegistrationActivity.class);//created intent object
-        startActivity(intent);  //start the activity
-    } **/
-//}
+            }
+        });
+     }
+
+
+}
